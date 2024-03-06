fe89602f76b6881bdeef4bc43941f60f501708f8
Ronnielangat <ronnielangat2020@gmail.com>
2024-03-06 22:01:01 +0300

    m

--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,32 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self,name = "John", job = "Admin"):
+        self.name = name
+        self.job = job
+
+    def get_name(self):
+        return self._name
+    
+    def set_name(self,name):
+        if (type(name) in (str)):
+            self._name = name.title()
+
+        else:
+            print( "Name must be string between 1 and 25 characters.")
+
+
+    name = property(get_name,set_name)
+
+    def get_job(self):
+        return self._job
+    
+    def set_job(self, job):
+        if job in APPROVED_JOBS:
+            self._job = job
+
+        else:
+            print("Job must be in list of approved jobs.")
+
+
+    name = property(get_name, set_name)
